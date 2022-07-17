5e62c4aeae8d0d82300cc2ba8fee4fe8f8ecb844
Rin-Kokubo <86641034+RinKokubo@users.noreply.github.com>
2022-07-17 21:31:15 +0900

    fix bug

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div className="desktop:mx-[300px] sp:mx-[15px]">
-    <h1 className="desktop:mt-[50px] sp:mt-[20px] desktop:text-[60px] sp:text-[30px]">Where is your classroom?</h1>
-    <div className="flex desktop:flex-row sp:flex-col desktop:items-center desktop:justify-center sp:items-start desktop:mt-[60px] sp:mt-[30px] sp:mx-[30px]">
+    <h1 className="desktop:mt-[50px] sp:mt-[13px] desktop:text-[60px] sp:text-[24px]">Where is your classroom?</h1>
+    <div className="flex desktop:flex-row sp:flex-col items-center justify-center desktop:mt-[60px] sp:mt-[20px] sp:mx-[30px]">
       <div className="flex">
         <label className="text-[24px] mr-[10px]">Day:</label>
         <select className="border border-gray-400 rounded-[5px] py-[5px] px-[20px]" v-model="setDay">
@@ -25,13 +25,14 @@
           <option>5th</option>
         </select>
       </div>
-      <button className="flex justify-center items-center px-4 py-1 bg-teal-500 text-white rounded-[3px] ml-[50px] font-bold hover:bg-teal-600 duration-300"
+      <button className="flex justify-center items-center px-4 py-1 bg-teal-500 text-white rounded-[3px] desktop:ml-[50px] sp:ml-[0px]
+                         font-bold hover:bg-teal-600 duration-300 desktop:mt-[0px] sp:mt-[20px]"
               v-on:click="setDP">
         Search
       </button>
     </div>
-    <div className="flex flex-wrap gap-y-5 my-10 justify-center ml-[20px]">
-    <p v-for="(item, index) in data" :key="index" className="text-[24px] w-[400px] flex justify-center items-center">
+    <div className="flex flex-wrap desktop:gap-y-5 sp:gap-y-2 desktop:my-10 sp:my-5 justify-center ml-[20px]">
+    <p v-for="(item, index) in data" :key="index" className="desktop:text-[24px] sp:text-[18px] desktop:w-[380px] sp:w-[160px] flex justify-center items-center">
       {{ item }}
     </p>
     </div>
