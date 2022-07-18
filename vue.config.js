e46e861963d7acb41663e1cec7c12625d279e9e1
Rin-Kokubo <86641034+RinKokubo@users.noreply.github.com>
2022-07-18 09:56:47 +0900

    fix bug

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    performance: {
+      maxAssetSize: 700000,
+      maxEntrypointSize: 700000,
+    }
+  } 
 })
